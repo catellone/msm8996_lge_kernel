ec53b845c35d6ab4de4a1149f778f6059bcb93c1
catellone <catellone94@gmail.com>
2021-12-17 15:33:25 +0100

    EAS-Y: v1 (Initial release)
    Changelog:
    
    Fixed boot on US models
    Fixed cam focus issue
    Fixed WiFi Hotspot on US models
    Fixed ZRAM
    Fixed AOD
    Fixed compilation issues
    Import v30b blobs for Input, Net and Media
    
    Update build.sh
    
    Update copy_finished.sh

--- copy_finished.sh
+++ copy_finished.sh
@@ -52,7 +52,7 @@ BANNER=${MK2DIR}/banner
 CLEAN_DIR() {
 	echo "Cleaning folder..."
 	rm -rf $DDIR
-	rm -f $RDIR/$OUTDIR/${DEVICE}_${VER}-mk2000.zip
+	rm -f $RDIR/$OUTDIR/${DEVICE}_${VER}-EAS-Y.zip
 }
 
 SETUP_DIR() {
@@ -111,7 +111,7 @@ COPY_KERNEL() {
 ZIP_UP() {
 	echo "Creating AnyKernel3 archive..."
 	cd $DDIR
-	zip -7qr $RDIR/$OUTDIR/${DEVICE}_${VER}-mk2000.zip * \
+	zip -7qr $RDIR/$OUTDIR/${DEVICE}_${VER}-EAS-Y.zip * \
 		|| ABORT "Failed to create zip archive"
 }
 
